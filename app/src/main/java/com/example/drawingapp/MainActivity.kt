658df3a0b304d8771b3daa9c339def586a23b5d8
Benson1198 <bensonpx1198@gmail.com>
2021-11-29 14:53:31 +0530

    Implemented Undo

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -78,6 +78,16 @@ class MainActivity : AppCompatActivity() {
             showBrushSizeSelectorDialog()
         }
 
+        val ibUndo:ImageButton = findViewById(R.id.ib_undo)
+        ibUndo.setOnClickListener {
+            drawingView?.onClickUndo()
+        }
+
+//        val ibRedo:ImageButton = findViewById(R.id.ib_redo)
+//        ibRedo.setOnClickListener {
+//            drawingView?.onClickRedo()
+//        }
+
         val ibGallery: ImageButton = findViewById(R.id.ib_gallery)
         ibGallery.setOnClickListener {
             requestStoragePermission()
